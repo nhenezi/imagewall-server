70b5dc5108f247c6c048543c701cf06a18cf0f54
Nikola Henezi <nikola.henezi@gmail.com>
2012-12-14 19:33:29 +0100

    str_replace removed, whitespaces fix

--- application/controllers/picture.php
+++ application/controllers/picture.php
@@ -3,37 +3,36 @@
 class Picture extends CI_Controller{
 
   public function __construct()
-	{
-		parent::__construct();
-		$this->load->helper(array('form', 'url'));
-  	$this->load->model('mymodel');
-	}
+  {
+    parent::__construct();
+    $this->load->helper(array('form', 'url'));
+    $this->load->model('mymodel');
+  }
 
   public function index()
   {
     $this->load->view('imagewall_site', array('error' => ' ','value' => 'HELLOOOOOOOOO','data' => array() ));
   }
-  
+
   public function loadWithPrefix($prefix = NULL)
   {
     if ($prefix == NULL)
     {
       $data = $this->mymodel->get_all();
       $query = array('error' => "No prefix",'value' =>"",'data' => $data );
-			$this->load->view('imagewall_site', $query);
+      $this->load->view('imagewall_site', $query);
     }
     else
-    { 
+    {
       $data = $this->mymodel->get_by_prefix($prefix);
       $this->load->view('imagewall_site', array('error' => ' ','value'=>' ' ,'data' => $data ));
     }
   }
   public function getLatest($id, $limit)
   {
-    $data = str_replace('\\/', '/',json_encode($this->mymodel->get_latest_news($id, $limit)));
     $this->output
     ->set_content_type('application/json')
-    ->set_output($data);
+    ->set_output(json_encode($this->mymodel->get_latest_news($id, $limit)));
 
 #    $this->load->view('imagewall_site', array('error' => ' ','value'=>' ' ,'data' => $data ));
   }
