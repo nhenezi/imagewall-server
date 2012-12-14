d8dbbe775673618dc9b302257ba780ec1a89eb8b
Nikola Henezi <nikola.henezi@gmail.com>
2012-12-14 11:31:00 +0100

    extra methods removed

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -41,35 +41,5 @@ class Upload extends CI_Controller {
       $this->load->view('upload_success', $data);
     }*/
   }
-
-	function index()
-	{
-		$this->load->view('upload_form', array('error' => ' ' ));
-	}
-
-	function do_upload()
-	{
-		$config['upload_path'] = './uploads/';
-		$config['allowed_types'] = 'gif|jpg|png';
-		$config['max_size']	= '100';
-		$config['max_width']  = '1024';
-		$config['max_height']  = '768';
-
-		$this->load->library('upload', $config);
-
-		if ( ! $this->upload->do_upload() )
-		{
-		  var_dump($this->upload->data())
-			//$error = array('error' => $this->upload->display_errors());
-
-			//$this->load->view('upload_form', $error);
-		}
-		else
-		{
-			$data = array('upload_data' => $this->upload->data());
-
-			$this->load->view('upload_success', $data);
-		}
-	}
 }
 ?>
