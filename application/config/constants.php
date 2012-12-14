32f2987801a8f77a473f9b0ea1c1e6f01bf21d75
Nikola Henezi <nikola.henezi@gmail.com>
2012-12-14 12:31:00 +0100

    UPLOADS constant defined

--- application/config/constants.php
+++ application/config/constants.php
@@ -36,6 +36,15 @@ define('FOPEN_READ_WRITE_CREATE',				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
+/*
+|--------------------------------------------------------------------------
+| Custom constants
+|--------------------------------------------------------------------------
+*/
+
+define('UPLOADS', base_url().'images/');
+
+
 
 /* End of file constants.php */
 /* Location: ./application/config/constants.php */
